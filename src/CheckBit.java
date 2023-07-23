f8057ad7a26f69b1bb6d7771d372f02c2f93d8b6
Mathiyarasip31 <mathiyarasip31@gmail.com>
2023-07-23 18:49:20 +0530

    Initial commit done

---
+++ src/CheckBit.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class CheckBit {
+
+    public static void checkBit(int number,int index)
+    {
+        if((number & (1 << index)) == 0)
+        {
+            System.out.println(0);
+        }
+        else
+        {
+            System.out.println(1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        int index = scan.nextInt();
+        checkBit(number,index);
+
+    }
+
+}
